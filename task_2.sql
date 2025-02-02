92b6b1f10834acd24bf97df1e969ea72f8d11c74
Rachel <rachelwanjiru037@gmail.com>
2025-02-02 23:57:02 +0300

    Final commit

--- task_2.sql
+++ task_2.sql
@@ -1,42 +1,31 @@
--- Create the Authors table
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
-
--- Create the Books table
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT PRIMARY KEY, 
+    author_name VARCHAR(215)
+    )
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT PRIMARY KEY, 
+    title VARCHAR(130),
     author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);
-
--- Create the Customers table
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT
-);
-
--- Create the Orders table
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    price FLOAT,
+    publication_date DATE
+    )
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT PRIMARY KEY, 
+    customer_name VARCHAR(215),
+    email VARCHAR(215), address TEXT
+    )
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
-  
-);
-
--- Create the Order_Details table
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+    )
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id INT, 
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id,
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
-
+    quantity DOUBLE,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
