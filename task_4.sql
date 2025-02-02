8a4f3750ef83e002923e64ae042e80e42d714be8
Rachel <rachelwanjiru037@gmail.com>
2025-02-02 23:36:02 +0300

    Add task_4.sql for printing full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,9 @@
-USE alx_book_store;
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, 
+       DATA_TYPE, 
+       CHARACTER_MAXIMUM_LENGTH, 
+       IS_NULLABLE, 
+       COLUMN_DEFAULT 
+FROM information_schema.columns 
+WHERE table_name = 'books' 
+  AND table_schema = 'alx_book_store';
+
